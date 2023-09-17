f978bfdd660410d2ebe8bb88ad5ea750d8b2a9a9
Rahul Dutta <rdtech2002@gmail.com>
2023-09-17 13:40:09 +0530

    .

--- 019-week-16-hof-functional-programming/answers/2.js
+++ 019-week-16-hof-functional-programming/answers/2.js
@@ -1,21 +1,22 @@
 function generateRandomNumber() {
-    return Math.floor(Math.random() * 100);
+  return Math.floor(Math.random() * 100);
 }
 
-
 function generateRandomNumberWithDelay(delayInSeconds) {
-    console.log(`Generating a random number after a delay of ${delayInSeconds} seconds...`);
-    let countdown = 3;
-    const countdownInterval = setInterval(() => {
-        console.log(`${countdown} seconds remaining...`);
-        countdown--;
+  console.log(
+    `Generating a random number after a delay of ${delayInSeconds} seconds...`
+  );
+  let countdown = 3;
+  const countdownInterval = setInterval(() => {
+    console.log(`${countdown} seconds remaining...`);
+    countdown--;
 
-        if (countdown === 0) {
-            clearInterval(countdownInterval);
-            const randomNumber = generateRandomNumber();
-            console.log(`Random number generated: ${randomNumber}`);
-        }
-    }, 1000);
+    if (countdown === 0) {
+      clearInterval(countdownInterval);
+      const randomNumber = generateRandomNumber();
+      console.log(`Random number generated: ${randomNumber}`);
+    }
+  }, 1000);
 }
 
 generateRandomNumberWithDelay(3);
--- 019-week-16-hof-functional-programming/answers/6.js
+++ 019-week-16-hof-functional-programming/answers/6.js
@@ -1,14 +1,14 @@
-const linkedinProfileRegex = /^https:\/\/www\.linkedin\.com\/in\/[a-zA-Z0-9_-]{5,30}[a-zA-Z0-9]\/$/;
+const linkedinProfileRegex =
+  /^https:\/\/www\.linkedin\.com\/in\/[a-zA-Z0-9_-]{5,30}[a-zA-Z0-9]\/$/;
 
 function isValidLinkedInProfileURL(url) {
-    return linkedinProfileRegex.test(url);
+  return linkedinProfileRegex.test(url);
 }
 
-let url = 'https://www.linkedin.com/in/mithun-s-83a8a3186/'
+let url = "https://www.linkedin.com/in/mithun-s-83a8a3186/";
 
 if (isValidLinkedInProfileURL(url)) {
-    console.log(`"${url}" is a valid LinkedIn profile URL.`);
+  console.log(`"${url}" is a valid LinkedIn profile URL.`);
 } else {
-    console.log(`"${url}" is not a valid LinkedIn profile URL.`);
+  console.log(`"${url}" is not a valid LinkedIn profile URL.`);
 }
-
